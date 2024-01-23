f35748c6b28a8ed2eba5f688514af6cadab4a00b
CaptainAlpha04 <aliimran.iam59@gmail.com>
2024-01-23 11:11:22 +0500

    Search + gpt integration added

--- app.mjs
+++ app.mjs
@@ -1,19 +1,19 @@
 import express from 'express'
 import OpenAI from 'openai'
 import path from 'path'
+import keys from './keys.js'
 
 const app = express()
 const port = 5000
 
 const openai = new OpenAI({
-    organization: 'YOUR_ORGANIZATION_KEY',
-    apiKey: 'YOUR_API_KEY',
+    apiKey: keys,
   });
   
-  async function getChatCompletion(p) {
+  async function getChatCompletion(prompt) {
     const stream = await openai.chat.completions.create({
       model: "gpt-3.5-turbo-16k",
-      messages: [{ role: "user", content: p}],
+      messages: [{ role: "user", content: prompt}],
       stream: true,
     });
   
@@ -36,9 +36,9 @@ app.get('/', (req, res) => {
 
 app.get('/quote', async (req, res) => {
   
-    const p = `You are a wise and helpful quote generator. your goal is to give quotes of the greatest people who ever lived as well as anonymous people. Please give only single quote at a time. give quotes on various and diverse topics. the quotes should be unique and new everytime and lesser known. the quotes should be short`;
+    const prompt = `You are a wise and helpful quote generator. your goal is to give quotes of the greatest people who ever lived as well as anonymous people. Please give only single quote at a time. give quotes on various and diverse topics. the quotes should be unique and new everytime and lesser known. the quotes should be short`;
     try {
-      const quote = await getChatCompletion(p);
+      const quote = await getChatCompletion(prompt);
       res.json({quote});
     } catch (error) {
       console.error('Error getting quote:', error.message);
@@ -46,6 +46,18 @@ app.get('/quote', async (req, res) => {
     }
 });
 
+app.get('/chatRes', async (req, res) => {
+
+    let prompt = req.query.prompt || "";
+    try {
+      const chat = await getChatCompletion(prompt);
+      res.json({chat});
+    }
+    catch (error){
+      console.error('Error gettign chat: ', error.message)
+      res.status(500).json({error: 'Unable to access ChatGPT! Server Error'});
+    }
+});
 
 app.listen(port, () => {
     console.log(`listening on port ${port}...`)
